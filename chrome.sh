da371c99290369ce3f957b461b3e3aeafa748ae7
Nick Jenkins <117663799+nickj1976@users.noreply.github.com>
2024-12-13 14:38:21 +0000

    Add files via upload

---
+++ chrome.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Add the Google Chrome repository
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
+
+# Update the package list
+apt update
+
+# Install Google Chrome Stable
+apt install -y google-chrome-stable
